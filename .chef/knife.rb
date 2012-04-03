274ab690bc1caec3aec786789fc7fba6aeb5609c
E Camden Fisher <fish@fishnix.net>
2012-04-03 19:03:51 -0400

    first

---
+++ .chef/knife.rb
@@ -0,0 +1,25 @@
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "fishnix-yale"
+client_key               "#{current_dir}/fishnix-yale.pem"
+validation_client_name   "fishnix-yale-validator"
+validation_key           "#{current_dir}/fishnix-yale-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/fishnix-yale"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
+# Rackspace:
+knife[:rackspace_api_key]  = "#{ENV['RACKSPACE_API_KEY']}"
+knife[:rackspace_api_username] = "#{ENV['RACKSPACE_API_USER']}"
+## EC2:
+knife[:aws_ssh_key_id] 		= "#{ENV['AWS_SSH_KEY_ID']}"
+knife[:aws_access_key_id]     	= "#{ENV['AWS_ACCESS_KEY_ID']}"
+knife[:aws_secret_access_key] 	= "#{ENV['AWS_SECRET_ACCESS_KEY']}"
+## Slicehost
+#knife[:slicehost_password] = "Your Slicehost API password"
+# 
+## Terremark
+#knife[:terremark_password] = "Your Terremark Password"
+#knife[:terremark_username] = "Your Terremark Username"
+#knife[:terremark_service]  = "Your Terremark Service name"
