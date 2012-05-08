43338700ca4e7e7440919902ffb0298dfdc8cb3a
E Camden Fisher <fish@fishnix.net>
2012-05-07 21:41:09 -0400

    manage system.properties + more sysconfig

--- cookbooks/apache-servicemix/attributes/default.rb
+++ cookbooks/apache-servicemix/attributes/default.rb
@@ -1,3 +1,15 @@
-default[:apache_servicemix][:servicemix_home]     = '/usr/local/apache-servicemix-4.4.1-fuse-03-06'
+default[:apache_servicemix][:servicemix_install]   = '/usr/local/apache-servicemix-4.4.1-fuse-03-06'
 default[:apache_servicemix][:servicemix_file]     = 'apache-servicemix-4.4.1-fuse-03-06.tar.gz'
 default[:apache_servicemix][:servicemix_url]      = 'https://s3.amazonaws.com/chef-misc/pkg/apache-servicemix-4.4.1-fuse-03-06.tar.gz'
+default[:apache_servicemix][:user]                = "appa"
+default[:apache_servicemix][:servicemix_home]     = "/usr/local/apache-servicemix"
+
+default[:apache_servicemix][:maxperm] = "128m"
+default[:apache_servicemix][:maxheap] = "1024m"
+default[:apache_servicemix][:minheap] = "128m"
+
+default[:apache_servicemix][:java_opts] = []
+
+default[:apache_servicemix][:instances] = { 'child00' => { },
+                                            'child01' => { }
+                                          }
--- cookbooks/apache-servicemix/recipes/default.rb
+++ cookbooks/apache-servicemix/recipes/default.rb
@@ -9,6 +9,11 @@
 
 include_recipe 'java'
 
+# add user if doesn't exist already
+user "#{node[:apache_servicemix][:user]}" do
+  comment "App User"
+end
+
 # Install jdk
 # This is total YUCK, but no yum repo for me
 # and I didn't want to build CENTOS 6.x pkgs anyways
@@ -16,15 +21,57 @@ include_recipe 'java'
 bash "install_servicemix" do
   user "root"
   cwd "/usr/local"
-  not_if { File.exists?("#{node[:apache_servicemix][:servicemix_home]}") }
+  not_if { File.exists?("#{node[:apache_servicemix][:servicemix_install]}") }
   code <<-EOH
   /usr/bin/wget "#{node[:apache_servicemix][:servicemix_url]}"
   /bin/tar -zxf "#{node[:apache_servicemix][:servicemix_file]}"
   /bin/rm -f "#{node[:apache_servicemix][:servicemix_file]}"
+  chown -Rh root:root #{node[:apache_servicemix][:servicemix_install]}
   EOH
 end
 
-link "/usr/local/apache-servicemix" do
-  to "#{node[:apache_servicemix][:servicemix_home]}"
-  only_if { File.exists("#{node[:apache_servicemix][:servicemix_home]}") }
+link "#{node[:apache_servicemix][:servicemix_home]}" do
+  to "#{node[:apache_servicemix][:servicemix_install]}"
+  only_if { File.exists?("#{node[:apache_servicemix][:servicemix_home]}") }
+end
+
+
+# create servicemix subdirs
+%w{ data deploy etc instances lock }.each do |d|
+  directory "#{node[:apache_servicemix][:servicemix_install]}/#{d}" do
+    owner "#{node[:apache_servicemix][:user]}"
+    group "#{node[:apache_servicemix][:user]}"
+    only_if { File.exists?("#{node[:apache_servicemix][:servicemix_install]}") }
+    mode "0755"
+    action :create
+  end
+end
+
+# system.properties
+template "#{node[:apache_servicemix][:servicemix_install]}/etc/system.properties" do
+    source "system.properties.erb"
+    owner "root"
+    group "root"
+    mode "0444"
+end
+
+# sysconfig
+template "/etc/sysconfig/apache-servicemix" do
+    source "apache-servicemix_sysconfig.erb"
+    owner "root"
+    group "root"
+    mode "0444"
+    variables(
+      :java_opts => node[:apache_servicemix][:java_opts],
+      :instances => node[:apache_servicemix][:instances].keys
+    )
+    #notifies :restart, resources(:service => "jboss_#{n}")
+end
+
+# init script
+template "/etc/init.d/apache-servicemix" do
+    source "apache-servicemix_init.erb"
+    owner "root"
+    group "root"
+    mode "0555"
 end
