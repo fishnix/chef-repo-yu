4fa1503aac24457567412e42c1a002025c581003
E Camden Fisher <fish@fishnix.net>
2012-06-18 15:33:22 -0400

    depends java metadata

--- cookbooks/apache-activemq/metadata.rb
+++ cookbooks/apache-activemq/metadata.rb
@@ -1,6 +1,7 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "E Camden Fisher"
+maintainer_email "fish@fishnix.net"
 license          "All rights reserved"
 description      "Installs/Configures apache-activemq"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+depends          "java"
