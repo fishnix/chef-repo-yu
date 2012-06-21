fdc1f8a3361e0d6ad7e7b206d0ba37075bcc5064
E Camden Fisher <fish@fishnix.net>
2012-06-21 19:48:38 -0400

    syntax error in directory resources

--- cookbooks/jenkins/recipes/default.rb
+++ cookbooks/jenkins/recipes/default.rb
@@ -7,21 +7,24 @@
 # All rights reserved - Do Not Redistribute
 #
 
+include_recipe 'java'
+
 user "#{node[:jenkins][:jenkins_user]}" do
+  comment "Applicatiion User"
   action :create
 end
 
 # Create dirs for jenkins
 directory "#{node[:jenkins][:jenkins_base]}" do
   action :create
-  user  "#{node[:jenkins][:jenkins_user]}"
+  owner  "#{node[:jenkins][:jenkins_user]}"
   group "#{node[:jenkins][:jenkins_user]}"
   mode "0755"
 end
 
 directory "#{node[:jenkins][:jenkins_home]}" do
   action :create
-  user  "#{node[:jenkins][:jenkins_user]}"
+  owner  "#{node[:jenkins][:jenkins_user]}"
   group "#{node[:jenkins][:jenkins_user]}"
   mode "0755"
 end
@@ -29,6 +32,8 @@ end
 # Install jenkins war file
 remote_file "#{node[:jenkins][:jenkins_home]}/jenkins.war" do
   source "#{node[:jenkins][:jenkins_url]}"
+  owner "#{node[:jenkins][:jenkins_user]}"
+  group "#{node[:jenkins][:jenkins_user]}"
   mode "0644"
   action :create_if_missing
 end
