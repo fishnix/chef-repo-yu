22cc29a1cbf3741fb4d601a19e31b0e381e510c4
E Camden Fisher <fish@fishnix.net>
2012-06-21 22:21:40 -0400

    jenkins init

--- cookbooks/jenkins/attributes/default.rb
+++ cookbooks/jenkins/attributes/default.rb
@@ -2,3 +2,5 @@ default[:jenkins][:jenkins_user]    = 'appa'
 default[:jenkins][:jenkins_base]    = '/usr/local/jenkins'
 default[:jenkins][:jenkins_home]    = '/usr/local/jenkins/JENKINS_HOME'
 default[:jenkins][:jenkins_url]     = 'http://mirrors.jenkins-ci.org/war-stable/latest/jenkins.war'
+default[:jemkins][:use_jetty]       = true
+default[:jenkins][:http_port]       = "8888"
