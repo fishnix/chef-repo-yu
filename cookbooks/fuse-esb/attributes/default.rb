2575c6eadb31a24b4d1e30a160e3a77d21aff78d
E Camden Fisher <fish@fishnix.net>
2012-06-23 15:46:58 -0400

    cleaned up attr names for fuse_esb cookbook

--- cookbooks/fuse-esb/attributes/default.rb
+++ cookbooks/fuse-esb/attributes/default.rb
@@ -1,8 +1,8 @@
-default[:fuse_esb][:fuse_esb_install]   = '/usr/local/fuse-esb-7.0.0.fuse-061'
-default[:fuse_esb][:fuse_esb_file]      = 'fuse-esb-full-7.0.0.fuse-061.tar.gz'
-default[:fuse_esb][:fuse_esb_url]       = 'https://s3.amazonaws.com/chef-misc/pkg/fuse-esb-full-7.0.0.fuse-061.tar.gz'
+default[:fuse_esb][:install]   = '/usr/local/fuse-esb-7.0.0.fuse-061'
+default[:fuse_esb][:file]      = 'fuse-esb-full-7.0.0.fuse-061.tar.gz'
+default[:fuse_esb][:url]       = 'https://s3.amazonaws.com/chef-misc/pkg/fuse-esb-full-7.0.0.fuse-061.tar.gz'
 default[:fuse_esb][:user]               = "appa"
-default[:fuse_esb][:fuse_esb_home]      = "/usr/local/fuse-esb"
+default[:fuse_esb][:home]      = "/usr/local/fuse-esb"
 
 default[:fuse_esb][:maxperm] = "128m"
 default[:fuse_esb][:maxheap] = "1024m"
