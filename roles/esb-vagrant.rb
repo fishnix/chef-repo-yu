99c3489cd58c175906587ae59ed0dcc9e4e0ef41
E Camden Fisher <fish@fishnix.net>
2012-06-23 13:21:33 -0400

    mq init + esb-vagrant role

---
+++ roles/esb-vagrant.rb
@@ -0,0 +1,17 @@
+name "esb-vagrant"
+description "Role to create a Fuse MQ server on vagrant"
+run_list(
+  "recipe[java]",
+  "recipe[fuse-esb]",
+  "recipe[jenkins]"
+)
+default_attributes(
+  "java" => { "jdk_url"   => 'http://leleupi.its.yale.edu:8181/PKG/jdk1.6.0_33.tar.gz',
+              "jdk_file"  => 'jdk1.6.0_33.tar.gz',
+              "java_home" => '/usr/local/jdk1.6.0_33'
+            },
+  "fuse-esb" => {   "url"   => 'http://leleupi.its.yale.edu:8181/PKG/fuse-esb-7.0.0.fuse-061.tar.gz',
+                    "file"  => 'fuse-esb-7.0.0.fuse-061.tar.gz',
+                    "home"  => '/usr/local/fuse-esb'
+              }
+)
