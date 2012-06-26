c15e9d1896294a17187756c61751bb1b80acbc5d
E Camden Fisher <fish@fishnix.net>
2012-06-26 15:48:05 -0400

    enable node00 for jboss role

--- roles/jboss-vagrant.rb
+++ roles/jboss-vagrant.rb
@@ -13,7 +13,8 @@ default_attributes(
   "jboss" => {  "jboss_url"   => 'http://leleupi.its.yale.edu:8181/PKG/jboss-eap-5.0.1.tar.gz',
                 "jboss_file"  => 'jboss-eap-5.0.1.tar.gz',
                 "jboss_home"  => '/usr/local/jboss-eap-5.0/jboss-as',
-                "jboss_apps"  => '/usr/local/jboss-apps'
+                "jboss_apps"  => '/usr/local/jboss-apps',
+                "nodes"       => { "node00" => { "node_enabled"  => true } }
               },
   "jenkins" => { "jenkins_url"  => 'http://leleupi.its.yale.edu:8181/PKG/jenkins.war' }
 )
