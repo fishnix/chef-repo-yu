8aa90bdfecce0a4041acf5f42ef85003795cf3a4
Nick Silkey <nick@silkey.org>
2012-07-03 16:44:08 -0400

    first pass at phpmyadmin and mysql-server cookbooks

---
+++ cookbooks/phpmyadmin/recipes/default.rb
@@ -0,0 +1,39 @@
+#
+# Cookbook Name:: phpmyadmin
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+if platform?("centos","redhat","fedora")
+  include_recipe "yum"
+end
+
+package "phpmyadmin" do
+  package_name "phpmyadmin"
+  action [ :install, :upgrade ]
+end
+
+service "httpd" do
+  service_name "httpd"
+  action :enable
+  supports :status => true, :restart => true, :stop => true, :start => true
+end
+
+template "/etc/httpd/conf.d/phpMyAdmin.conf" do
+    source  "phpMyAdmin.conf.erb"
+    owner   "root"
+    group   "root"
+    mode    "0444"
+    notifies :restart, resources(:service => "httpd"), :delayed
+end
+
+template "/etc/phpMyAdmin/config.inc.php" do
+    source  "config.inc.php.erb"
+    owner   "root"
+    group   "root"
+    mode    "0444"
+    notifies :restart, resources(:service => "httpd"), :delayed
+end
---
+++ roles/mysql-server.rb
@@ -0,0 +1,5 @@
+name "mysql-server"
+description "Role to create a mysql-server"
+run_list(
+  "recipe[mysql-server]"
+)
