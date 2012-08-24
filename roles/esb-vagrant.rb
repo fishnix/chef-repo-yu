8db0f767489676d92de6fcc09dfed3b847e63094
E Camden Fisher <fish@fishnix.net>
2012-08-24 11:33:19 -0400

    fix path to fuse esb blog

--- roles/esb-vagrant.rb
+++ roles/esb-vagrant.rb
@@ -1,6 +1,7 @@
 name "esb-vagrant"
 description "Role to create a Fuse MQ server on vagrant"
 run_list(
+  "recipe[misc::hosts]",
   "recipe[java]",
   "recipe[fuse-esb]",
   "recipe[jenkins]"
@@ -10,8 +11,8 @@ default_attributes(
               "jdk_file"  => 'jdk1.6.0_33.tar.gz',
               "java_home" => '/usr/local/jdk1.6.0_33'
             },
-  "fuse_esb" => {   "url"   => 'http://leleupi.its.yale.edu:8181/PKG/fuse-esb-7.0.0.fuse-061.tar.gz',
-                    "file"  => 'fuse-esb-7.0.0.fuse-061.tar.gz',
+  "fuse_esb" => {   "url"   => 'http://leleupi.its.yale.edu:8181/PKG/fuse-esb-full-7.0.0.fuse-061.tar.gz',
+                    "file"  => 'fuse-esb-full-7.0.0.fuse-061.tar.gz',
                     "home"  => '/usr/local/fuse-esb'
               }
 )
