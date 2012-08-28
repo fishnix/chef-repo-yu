3a2b5b170eafb283f6377d481bd000c4d90e47e1
E Camden Fisher <fish@fishnix.net>
2012-08-28 10:26:42 -0400

    cleaned out spurious attributes in esb-vagrant role

--- roles/esb-vagrant.rb
+++ roles/esb-vagrant.rb
@@ -7,13 +7,4 @@ run_list(
   "recipe[jenkins]"
 )
 default_attributes(
-  "java" => { "jdk_url"   => 'http://leleupi.its.yale.edu:8181/PKG/jdk1.6.0_33.tar.gz',
-              "jdk_file"  => 'jdk1.6.0_33.tar.gz',
-              "java_home" => '/usr/local/jdk1.6.0_33'
-            },
-  "fuse_esb" => {   "url"   => 'http://leleupi.its.yale.edu:8181/PKG/fuse-esb-full-7.0.0.fuse-061.tar.gz',
-                    "file"  => 'fuse-esb-full-7.0.0.fuse-061.tar.gz',
-                    "home"  => '/usr/local/fuse-esb',
-                    "user"  => 'vagrant'
-              }
 )
