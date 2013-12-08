fe78377898a15fc588a3b70c263620c23e65e090
E Camden Fisher <fish@fishnix.net>
2013-12-08 17:32:29 -0500

    blasted old apache2 cookbook

--- cookbooks/apache2.old/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "E Camden Fisher"
-maintainer_email "fish@fishnix.net"
-license          "Apache 2.0"
-description      "Installs/Configures apache2"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
