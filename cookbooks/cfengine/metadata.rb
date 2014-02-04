81cfa1b47a71bd8772224687435d0b80f7b1989d
E Camden Fisher <fish@fishnix.net>
2014-02-04 18:26:26 -0500

    started adding test kitchen

--- cookbooks/cfengine/metadata.rb
+++ cookbooks/cfengine/metadata.rb
@@ -1,6 +1,7 @@
 maintainer       "E Camden Fisher"
 maintainer_email "camden.fisher@yale.edu"
 license          "All rights reserved"
+name			 "cfengine"
 description      "Installs/Configures cfengine"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
--- cookbooks/yum/metadata.rb
+++ cookbooks/yum/metadata.rb
@@ -1,6 +1,7 @@
 maintainer       "E Camden Fisher"
 maintainer_email "fish@fishnix.net"
 license          "Apache 2.0"
+name			 "yum"
 description      "Installs/Configures yum"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
