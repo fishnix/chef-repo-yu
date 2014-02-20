aae321b9cc332e9c1038e1b8f8c9b26a89649495
E Camden Fisher <fish@fishnix.net>
2014-02-20 10:11:16 -0500

    delete .old dir... duh

--- python.old/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "E Camden Fisher"
-maintainer_email "camden.fisher@yale.edu"
-license          "All rights reserved"
-description      "Installs/Configures python"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
